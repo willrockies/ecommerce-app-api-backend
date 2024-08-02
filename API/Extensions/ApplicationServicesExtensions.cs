29766a9fa3c81b2e8fbf99bbeedb2fb6730bd0f6
Wilson Alves da Silva <wlsn.ads@gmail.com>
2024-08-02 10:18:02 -0300

    Creating a basket repository interface

--- API/Extensions/ApplicationServicesExtensions.cs
+++ API/Extensions/ApplicationServicesExtensions.cs
@@ -24,6 +24,7 @@ namespace API.Extensions
 
                 return ConnectionMultiplexer.Connect(options);
             });
+            services.AddScoped<IBasketRepository, BasketRepository>();
             services.AddScoped<IProductRepository, ProductRepository>();
             services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
             services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
--- Core/Entities/CustomerBasket.cs
+++ Core/Entities/CustomerBasket.cs
@@ -8,10 +8,7 @@ namespace Core.Entities
 {
     public class CustomerBasket
     {
-        public CustomerBasket()
-        {
-            
-        }
+        public CustomerBasket() { }
 
         public CustomerBasket(string id)
         {
@@ -21,6 +18,6 @@ namespace Core.Entities
         public string Id { get; set; }
         public List<BasketItem> Items { get; set; } = new List<BasketItem>();
 
-      
+
     }
 }
