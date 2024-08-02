e76f29f7d5322260200931611e850c57945f0771
Wilson Alves da Silva <wlsn.ads@gmail.com>
2024-08-02 15:18:29 -0300

    [*]esqueci de colocar a propriedade de Brand

--- Core/Entities/BasketItem.cs
+++ Core/Entities/BasketItem.cs
@@ -7,6 +7,7 @@
         public decimal Price { get; set; }
         public int Quantity { get; set; }
         public string PictureUrl { get; set; }
+        public string Brand { get; set; }
         public string Type { get; set; }
     }
 }
