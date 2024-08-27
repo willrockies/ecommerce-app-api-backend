5804c0ebbd0db88467db67d55d6a9d9ec51cadfc
Wilson Alves da Silva <wlsn.ads@gmail.com>
2024-08-27 16:09:16 -0300

    Checking for duplicate email addresses

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -72,7 +72,7 @@ namespace API.Controllers
 
             var result = await _userManager.UpdateAsync(user);
 
-            if(result.Succeeded) return Ok(_mapper.Map<Address, AddressDto>(user.Address));
+            if (result.Succeeded) return Ok(_mapper.Map<Address, AddressDto>(user.Address));
 
             return BadRequest("Problem Updating the user");
         }
@@ -97,6 +97,13 @@ namespace API.Controllers
         [HttpPost("register")]
         public async Task<ActionResult<UserDto>> Register(RegisterDto registerDto)
         {
+            if (CheckEmailExistsASync(registerDto.Email).Result.Value)
+            {
+                return new BadRequestObjectResult(new ApiValidationErrorResponse
+                {
+                    Errors = new[]
+                { "Email address is in use" }});
+            }
             var user = new AppUser
             {
                 DisplayName = registerDto.DisplayName,
